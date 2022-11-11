d767d74756b87a60d1f79538924bc8873b49c54b
Maksimchik36 <Peltek1985@gmail.com>
2022-11-11 10:53:02 +0200

    add comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,13 +3,18 @@ const { UserCtrl } = require('../controllers');
 
 const router = Router();
 // в процессе
-
+// створити ендпоінт для отримання:- особистої інформації про користувача, - інформації про тварин корисувача
 router.get('/:id', UserCtrl.getUserData);
+// router.get('/current', authenticate, ctrlWrapper(ctrl.getCurrent));
 
-router.post('/:id', UserCtrl.updateUserData);
 
-router.post('/', UserCtrl.addUserPet);
+// створити ендпоінт для оновлення одного з полів контактної інформації про користувача
+router.patch('/:id', authenticate, UserCtrl.updateUserData);
 
-router.post('/', UserCtrl.deleteUserPet);
+// створити ендпоінт для додавання карточки тварини користувача
+router.post('/', authenticate, UserCtrl.addUserPet);
+
+// створити ендпоінт для видалення карточки з твариною користувача
+router.delete('/', authenticate, UserCtrl.deleteUserPet);
 
 module.exports = router;
